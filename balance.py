742a855ba576a8cb459871446680beae7643bcd4
Lev Livnev <lev@liv.nev.org.uk>
2018-08-28 00:05:15 +0100

    basic balancing by simulated annealing.

---
+++ balance.py
@@ -0,0 +1,66 @@
+import numpy as np
+import scipy as sp
+
+def neighbour(xs, sigma=0.1):
+    # geometric brownian step
+    new_xs = xs * (1 + np.random.normal(0, sigma, size=len(xs)))
+    return new_xs
+
+def optimise_by_annealing(f, n,
+                          init=None,
+                          scale=lambda x: x,
+                          tol=1,
+                          max_iter=1000,
+                          init_temperature=100.0,
+                          cool=0.999):
+    if init == None:
+        # make some initial starting values:
+        xs = scale(np.ones(n))
+    else:
+        xs = scale(init)
+    temperature = init_tempterature
+    for i in range(max_iter):
+        loss = f(xs)
+        if loss < tol:
+            break
+        candidate = scale(neighbour(xs))
+        candidate_loss = f(candidate)
+        if candidate_loss < loss:
+            xs = candidate
+        else:
+            if np.random.uniform() < np.exp(-(candidate_loss - current_loss) / temperature):
+                xs = candidate
+        temperature *= cool
+    return xs, current_loss
+
+def cumulative_normal(x, mu=0, sigma=1.0):
+    return 0.5 * (1 + sp.special.erf((x - mu)/(np.sqrt(2) * sigma)))
+
+def liquidation_probability(ink, tab, tag, mat, sigma):
+    # assumes normal distribution of returns over period
+    # return to liquidation:
+    hit = mat / (ink * tag / tab) - 1
+    return cumulative_normal(hit, sigma=sigma)
+
+def make_risk_parity_objective(inks, tags, mats, sigmas):
+    N = len(inks)
+    lp = lambda tab, i: liquidation_probability(inks[i], tab, tags[i], mats[i], sigmas[i])
+    f = lambda tabs: sum([sum([abs(lp(tabs[i], i) - lp(tabs[j], j)) for j in range(0, i)]) for i in range(0, N)])
+    return f
+
+# ETH and DGX
+
+tags = [420., 38.]
+inks = [10., 300.]
+mats = [1.5, 1.1]
+sigmas = [0.3, 0.1]
+
+f = make_risk_parity_objective(inks, tags, mats, sigmas)
+
+# target debt: 3000 dai
+D = 3000
+
+optimise_by_annealing(f, 2, scale=(lambda tabs: D * tabs / sum(tabs)),
+                      tol=1.0)
+                      
+                      
