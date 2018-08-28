b2f0192d3be7414c28791ff2774e7c1b6c280dd0
Lev Livnev <lev@liv.nev.org.uk>
2018-08-28 22:53:54 +0100

    proper tangle export and edit README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cdp-balance
+
+nice html preview [here](https://htmlpreview.github.io/?https://github.com/livnev/cdp-balance/blob/master/balance.html).
+
+
+## requirements
+You'll need to have:
+`python`
+`numpy`
+`scipy`
--- balance.py
+++ balance.py
@@ -1,5 +1,22 @@
+
+import scipy.special
+
+def cumulative_normal(x, mu=0, sigma=1.0):
+    return 0.5 * (1 + scipy.special.erf((x - mu)/(np.sqrt(2) * sigma)))
+
+def liquidation_probability(ink, tab, tag, mat, sigma):
+    # assumes normal distribution of returns over period
+    # return to liquidation:
+    hit = mat / (ink * tag / tab) - 1
+    return cumulative_normal(hit, sigma=sigma)
+
+def make_risk_parity_objective(inks, tags, mats, sigmas):
+    N = len(inks)
+    lp = lambda tab, i: liquidation_probability(inks[i], tab, tags[i], mats[i], sigmas[i])
+    f = lambda tabs: sum([sum([abs(lp(tabs[i], i) - lp(tabs[j], j)) for j in range(0, i)]) for i in range(0, N)])
+    return f
+
 import numpy as np
-import scipy.special as sps
 
 def neighbour(xs, sigma=0.1):
     # geometric brownian step
@@ -34,40 +51,20 @@ def optimise_by_annealing(f, n,
     print("Ran annealing for {} iterations.".format(i+1))
     return xs, loss
 
-def cumulative_normal(x, mu=0, sigma=1.0):
-    return 0.5 * (1 + sps.erf((x - mu)/(np.sqrt(2) * sigma)))
-
-def liquidation_probability(ink, tab, tag, mat, sigma):
-    # assumes normal distribution of returns over period
-    # return to liquidation:
-    hit = mat / (ink * tag / tab) - 1
-    return cumulative_normal(hit, sigma=sigma)
-
-def make_risk_parity_objective(inks, tags, mats, sigmas):
-    N = len(inks)
-    lp = lambda tab, i: liquidation_probability(inks[i], tab, tags[i], mats[i], sigmas[i])
-    f = lambda tabs: sum([sum([abs(lp(tabs[i], i) - lp(tabs[j], j)) for j in range(0, i)]) for i in range(0, N)])
-    return f
-
 def do_example_with_2():
-     # ETH and DGX
+    # ETH and DGX
+    gems = ["ETH", "DGX"]
+    tags = [420., 38.]
+    inks = [10., 100.]
+    mats = [1.5, 1.1]
+    sigmas = [0.2, 0.1]
+    f = make_risk_parity_objective(inks, tags, mats, sigmas)
+    # target debt: 3000 dai
+    D = 3000
+    tabs, loss = optimise_by_annealing(f, 2, scale=(lambda tabs: D * tabs / sum(tabs)), tol=.0000001, max_iter=100000)
+    print("Results:")
+    for i, gem in enumerate(gems):
+      print("{} CDP: tab={}".format(gem, tabs[i]))
+      print("(collateral ratio={}, liquidation probability={})".format(inks[i]*tags[i]/tabs[i], liquidation_probability(inks[i], tabs[i], tags[i], mats[i], sigmas[i])))
 
-     gems = ["ETH", "DGX"]
-
-     tags = [420., 38.]
-     inks = [10., 100.]
-     mats = [1.5, 1.1]
-     sigmas = [0.2, 0.1]
-
-     f = make_risk_parity_objective(inks, tags, mats, sigmas)
-
-     # target debt: 3000 dai
-     D = 3000
-
-     tabs, loss = optimise_by_annealing(f, 2, scale=(lambda tabs: D * tabs / sum(tabs)),
-                                        tol=.0000001, max_iter=100000)
-
-     print("Results:")
-     for i, gem in enumerate(gems):
-         print("{} CDP: tab={}".format(gem, tabs[i]))
-         print("(collateral ratio={}, liquidation probability={})".format(inks[i]*tags[i]/tabs[i], liquidation_probability(inks[i], tabs[i], tags[i], mats[i], sigmas[i])))
+do_example_with_2()
