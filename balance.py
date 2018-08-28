5e5bb1effb6398d520656791d067d4103d261086
Lev Livnev <lev@liv.nev.org.uk>
2018-08-28 16:16:35 +0100

    put into org-mode blocks.

--- balance.py
+++ balance.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp
+import scipy.special as sps
 
 def neighbour(xs, sigma=0.1):
     # geometric brownian step
@@ -18,7 +18,7 @@ def optimise_by_annealing(f, n,
         xs = scale(np.ones(n))
     else:
         xs = scale(init)
-    temperature = init_tempterature
+    temperature = init_temperature
     for i in range(max_iter):
         loss = f(xs)
         if loss < tol:
@@ -28,13 +28,14 @@ def optimise_by_annealing(f, n,
         if candidate_loss < loss:
             xs = candidate
         else:
-            if np.random.uniform() < np.exp(-(candidate_loss - current_loss) / temperature):
+            if np.random.uniform() < np.exp(-(candidate_loss - loss) / temperature):
                 xs = candidate
         temperature *= cool
-    return xs, current_loss
+    print("Ran annealing for {} iterations.".format(i+1))
+    return xs, loss
 
 def cumulative_normal(x, mu=0, sigma=1.0):
-    return 0.5 * (1 + sp.special.erf((x - mu)/(np.sqrt(2) * sigma)))
+    return 0.5 * (1 + sps.erf((x - mu)/(np.sqrt(2) * sigma)))
 
 def liquidation_probability(ink, tab, tag, mat, sigma):
     # assumes normal distribution of returns over period
@@ -48,19 +49,25 @@ def make_risk_parity_objective(inks, tags, mats, sigmas):
     f = lambda tabs: sum([sum([abs(lp(tabs[i], i) - lp(tabs[j], j)) for j in range(0, i)]) for i in range(0, N)])
     return f
 
-# ETH and DGX
+def do_example_with_2():
+     # ETH and DGX
 
-tags = [420., 38.]
-inks = [10., 300.]
-mats = [1.5, 1.1]
-sigmas = [0.3, 0.1]
+     gems = ["ETH", "DGX"]
 
-f = make_risk_parity_objective(inks, tags, mats, sigmas)
+     tags = [420., 38.]
+     inks = [10., 100.]
+     mats = [1.5, 1.1]
+     sigmas = [0.2, 0.1]
 
-# target debt: 3000 dai
-D = 3000
+     f = make_risk_parity_objective(inks, tags, mats, sigmas)
 
-optimise_by_annealing(f, 2, scale=(lambda tabs: D * tabs / sum(tabs)),
-                      tol=1.0)
-                      
-                      
+     # target debt: 3000 dai
+     D = 3000
+
+     tabs, loss = optimise_by_annealing(f, 2, scale=(lambda tabs: D * tabs / sum(tabs)),
+                                        tol=.0000001, max_iter=100000)
+
+     print("Results:")
+     for i, gem in enumerate(gems):
+         print("{} CDP: tab={}".format(gem, tabs[i]))
+         print("(collateral ratio={}, liquidation probability={})".format(inks[i]*tags[i]/tabs[i], liquidation_probability(inks[i], tabs[i], tags[i], mats[i], sigmas[i])))
